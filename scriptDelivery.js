28261643e0ef2ac30499999db52063c6ad424808
Deanna-Tyner <angelbaby4mom@gmail.com>
2025-06-30 18:47:39 -0700

    power ups and enemy intervals functional

--- scriptDelivery.js
+++ scriptDelivery.js
@@ -1,4 +1,13 @@
 var myGamePiece;
+var flyingEnemy = [];
+var walkingEnemy = [];
+var lives;
+var livesRemain = 3;
+var jumpPowerUp = [];
+var sheildPowerUp = [];
+var storedPowerUp = [];
+var cooldown = 0;
+var invincCooldown = 0;
 
 
 
@@ -12,18 +21,27 @@ function startBaseGame() {
     document.getElementById("cred").style.display = "none";       
     document.getElementById("main").style.display = "block";
     document.getElementById("credButton").style.display = "none";
-    myGamePiece = new component(30, 30, "red", 0, 240);
+    myGamePiece = new component(100, 100, "red", 0, 450);
+    /*flyingEnemy = new enemy(100, 80, "green", 300, 150);
+    walkingEnemy = new enemy(100, 100, "blue", 300, 450);*/
+    lives = new livesCounter ("30px", "Consolas", "black", 280, 40, "text");
+    //jumpPowerUp = new powerUp(50, 50, "purple", 200, 400, "jump");
+    //sheildPowerUp = new powerUp(50, 50, "pink", 100, 400, "sheild");
     myGameArea.start();
+  
+
+
    
 }
 
 var myGameArea = {
     canvas : document.getElementById("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 500;
+        this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNum = 0;
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
             myGameArea.key = e.keyCode;
@@ -40,6 +58,109 @@ var myGameArea = {
     }
 }
 
+function enemy(width, height, color, x, y, type)
+{
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;  
+
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    }
+
+        this.newPos = function() {
+        
+        this.x += this.speedX;
+        this.y += this.speedY;
+        }
+}
+
+/*function fly(width, height, color, x, y, type)
+{
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;  
+
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+}
+
+function walk(width, height, color, x, y, type)
+{
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;  
+
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }  
+}*/
+
+
+function powerUp(width, height, color, x, y, type)
+{
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;  
+    this.powerUpActive = true;
+    this.visible = true;
+
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    }
+
+    this.powerInStorage = function(){
+
+        if(storedPowerUp.length == 0)
+        {
+            storedPowerUp.push(type);
+            this.active = false;
+            console.log(this.type + " has been stored");
+            //console.log(storedPowerUp.length);
+        }
+
+    }
+
+    this.newPos = function() {
+        
+        this.x += this.speedX;
+        this.y += this.speedY;
+        }
+
+
+}
+
+
+
+
 function component(width, height, color, x, y, type) {
     this.type = type;
     this.width = width;
@@ -51,10 +172,13 @@ function component(width, height, color, x, y, type) {
     this.gravity = 0.2;
     this.gravitySpeed = 0;
     this.jumping = false;
+    this.shrink = false;
     this.update = function() {
         ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        
     }
     this.newPos = function() {
         this.gravitySpeed += this.gravity;
@@ -79,18 +203,341 @@ function component(width, height, color, x, y, type) {
 
 
 
+function livesCounter(width, height, color, x, y, type)
+{
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;
+
+    this.update = function()
+    {
+        ctx = myGameArea.context;
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }  
+    }
+    
+
+}
+
+
+
+function lifeLost(invincible)
+{
+
+    //var invincible = false;
+
+    if(livesRemain > 0 && invincible == false)
+    {
+        livesRemain--;
+        cooldown = 80;
+    }
+    else if(invincible == true)
+    {
+        cooldown = 500;
+    }
+    
+    if (livesRemain == 0){
+
+        myGameArea.stop();
+        return;
+
+    }
+}
+
+
+function collision(gamePiece, enemy)
+{
+    var crash = true;
+
+    if(
+        ((gamePiece.y + gamePiece.height) < (enemy.y)) ||
+
+        (gamePiece.y > (enemy.y + enemy.height)) ||
+
+        ((gamePiece.x + gamePiece.width) < enemy.x) ||
+
+        (gamePiece.x > (enemy.x + enemy.width))
+    
+    )
+    {
+        crash = false;
+    }
+    else
+    {
+        return crash;
+    }
+}
+
+function cooldownTime()
+{
+    if(cooldown > 0)
+    {
+        cooldown--;
+    }    
+}
+
+
+
+function powerUpCollision(gamePiece, powerUpType)
+{
+    var powered = true;
+
+    if(
+        ((gamePiece.y + gamePiece.height) < (powerUpType.y)) ||
+
+        (gamePiece.y > (powerUpType.y + powerUpType.height)) ||
+
+        ((gamePiece.x + gamePiece.width) < powerUpType.x) ||
+
+        (gamePiece.x > (powerUpType.x + powerUpType.width))
+    
+    )
+    {
+        powered = false;
+    }
+    else
+    {
+        
+        return powered;
+    }   
+}
+
+function enemyInterval()
+{
+    flyingEnemy.push( new enemy(100, 80, "green", 300, 150));
+    //walkingEnemy.push( new enemy(100, 100, "blue", 300, 450));
+}
+
 function updateGameArea() {
+
+
+    cooldownTime();
+
+    let inter = Math.floor((Math.random() * 10000) + 9000);
+    
+
+    for(i = 0; i < flyingEnemy.length; i +=1)
+    {
+        if(collision(myGamePiece, flyingEnemy[i]) == true && cooldown == 0)
+            {
+                lifeLost(false);
+                console.log("hit flying enemy");
+
+            }        
+    }
+
+    for(i = 0; i < walkingEnemy.length; i+= 1)
+    {
+        if(collision(myGamePiece, walkingEnemy[i]) == true && cooldown == 0)
+        {
+                lifeLost(false);
+                console.log("hit walking enemy");
+        }
+    }
+
+
+    for(i = 0; i < jumpPowerUp.length; i += 1)
+    {
+        if(powerUpCollision(myGamePiece, jumpPowerUp[i]) == true && jumpPowerUp[i].powerUpActive)
+            {
+                jumpPowerUp[i].powerInStorage();
+                //jumpPowerUp.visible = false;
+                console.log("jump collision");
+            }
+    }
+
+    for(i = 0; i < sheildPowerUp.length; i += 1)
+    {
+        if(powerUpCollision(myGamePiece, sheildPowerUp[i]) == true && cooldown == 0)
+            {
+                lifeLost(true);
+                console.log("sheild used");
+            }
+    }
+
+   
+
+
     myGameArea.clear();
 
-    if (myGameArea.key == 37) {myGamePiece.speedX = -2; }
-    else if (myGameArea.key == 39) {myGamePiece.speedX = 2; }
-    if (myGameArea.key == 38 && !myGamePiece.jumping) {myGamePiece.gravitySpeed = -6.5; myGamePiece.jumping = true; }
+    myGameArea.frameNum += 1;
+
+    if(myGameArea.frameNum == 1 || everyFlyinterval(350))
+    {
+        flyingEnemy.push( new enemy(100, 100, "green", 500, 150));
+        //walkingEnemy.push( new enemy(100, 100, "blue", 300, 450));
+
+        //setInterval(enemyInterval, inter);
+    }
+   if(myGameArea.frameNum == 1 || everyWalkinterval(350))
+    {
+        walkingEnemy.push( new enemy(100, 100, "blue", 700, 400));
+    }
+
+    if(myGameArea.fameNum == 1 || everySheildInterval(350))
+    {
+        sheildPowerUp.push( new powerUp(50, 50, "pink", 900, 400, "sheild"));
+    }
+
+    if(myGameArea.frameNum == 1 || everyJumpInterval(350))
+    {
+        jumpPowerUp.push(new powerUp(50, 50, "purple", 200, 400, "jump"));
+    }
+    
 
 
+    for(i = 0; i < flyingEnemy.length; i += 1)
+    {
+        flyingEnemy[i].speedX = -1;
+        flyingEnemy[i].newPos();
+        flyingEnemy[i].update();
+    }
+
+    for(i = 0; i < walkingEnemy.length; i += 1)
+    {
+        walkingEnemy[i].speedX = -1;
+        walkingEnemy[i].newPos();
+        walkingEnemy[i].update();
+    }
+
+    for(i = 0; i < sheildPowerUp.length; i += 1)
+    {
+        sheildPowerUp[i].speedX = -1;
+        sheildPowerUp[i].newPos();
+        sheildPowerUp[i].update();
+    }
+
+    for(i = 0; i < jumpPowerUp.length; i += 1)
+    {
+        jumpPowerUp[i].speedX = -1;
+        jumpPowerUp[i].newPos();
+        jumpPowerUp[i].update();
+    }
+
+    move(myGameArea.key, "sim");
+    //move(myGameArea.key, "en");
+
+    
+    //flyingEnemy.update();
+    //walkingEnemy.update();
+    //console.log(myGameArea.frameNum);
+    //jumpPowerUp.update();
+    //sheildPowerUp.update();
+    myGamePiece.x += myGamePiece.speedX;
+    myGamePiece.y += myGamePiece.speedY;
+    lives.text = "Lives: " + livesRemain;
+    lives.update();
     myGamePiece.newPos();    
     myGamePiece.update();
 }
 
+function move(key)
+{
+    if(key == 37)
+    {
+        goLeft()
+    }
+    else if (key == 39)
+    {
+        goRight();
+    }
+
+    if(key == 38 && !myGamePiece.jumping)
+    {
+        if(storedPowerUp.length > 0)
+        {
+            if(storedPowerUp[0] == "jump")
+                {
+                    jump("en");
+                    storedPowerUp.pop();
+                }
+        }
+        else
+        {
+            jump("sim");
+        }
+        
+    }
+
+}
+
+function goLeft()
+{
+    myGamePiece.speedX = -2;
+}
+
+function goRight()
+{
+    myGamePiece.speedX = 2;
+}
+
+function jump(type)
+{
+    if(type == "en")
+    {
+        myGamePiece.gravitySpeed = -9.5; myGamePiece.jumping = true;
+
+        
+    }
+    else if (type == "sim")
+    {
+        myGamePiece.gravitySpeed = -6.5; myGamePiece.jumping = true;
+    }
+        
+  
+     
+}
+
+function superJump()
+{
+    myGamePiece.gravitySpeed = -9.5; myGamePiece.jumping = true;    
+}
+
+
+
+function everyFlyinterval(n) {
+    if ((myGameArea.frameNum / n) % 1 == 0) 
+        {
+            return true;
+        }
+        return false;
+}
+
+function everyWalkinterval(n) {
+    if ((myGameArea.frameNum / n) % 3 == 0) 
+        {
+            return true;
+        }
+        return false;
+}
+
+function everySheildInterval(n)
+{
+    if((myGameArea.frameNum / n) % 1 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+function everyJumpInterval(n)
+{
+    if((myGameArea.frameNum / n) % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
 
 var showCredits = function(){
        document.getElementById("head").style.display = "none";
@@ -101,7 +548,7 @@ var showCredits = function(){
        document.getElementById("bkButton").style.display = "block";
      };
       
-    var goBack = function(){
+var goBack = function(){
       document.getElementById("bkButton").style.display = "none";
       document.getElementById("cred").style.display = "none";
       document.getElementById("head").style.display = "block";
