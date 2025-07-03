206a5ee94e56a9047ed4c4e370bbb85950617f6f
Deanna-Tyner <angelbaby4mom@gmail.com>
2025-07-02 17:30:21 -0700

    sprites and end of game added

--- scriptDelivery.js
+++ scriptDelivery.js
@@ -8,6 +8,7 @@ var sheildPowerUp = [];
 var storedPowerUp = [];
 var cooldown = 0;
 var invincCooldown = 0;
+var endOfLevel = null;
 
 
 
@@ -20,24 +21,50 @@ function startBaseGame() {
         
     document.getElementById("cred").style.display = "none";       
     document.getElementById("main").style.display = "block";
+
+    document.getElementById("menu").style.display = "none";
     document.getElementById("credButton").style.display = "none";
-    myGamePiece = new component(100, 100, "red", 0, 450);
-    /*flyingEnemy = new enemy(100, 80, "green", 300, 150);
-    walkingEnemy = new enemy(100, 100, "blue", 300, 450);*/
+    myGamePiece = new component(100, 150, "Molly-Stand.png", 0, 450, "image");
+    background = new animateBackground(900, 500, 'DeliveryDelivery-Background.png', 0, 0, "back");
+
     lives = new livesCounter ("30px", "Consolas", "black", 280, 40, "text");
-    //jumpPowerUp = new powerUp(50, 50, "purple", 200, 400, "jump");
-    //sheildPowerUp = new powerUp(50, 50, "pink", 100, 400, "sheild");
+ 
+    setTimeout(() => {endOfLevel = new end(700, 700, "End-House.png", 850, -200, "image");}, 150000)
+
     myGameArea.start();
-  
+
 
 
    
 }
 
+function startEndlessMode() {
+
+    
+    document.getElementById("bGame").style.display = "none";
+    document.getElementById("endless").style.display = "none";
+    document.getElementById("head").style.display = "none";
+        
+    document.getElementById("cred").style.display = "none";       
+    document.getElementById("main").style.display = "block";
+
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("credButton").style.display = "none";
+    myGamePiece = new component(100, 150, "Molly-Stand.png", 0, 450, "image");
+    background = new animateBackground(900, 500, 'DeliveryDelivery-Background.png', 0, 0, "back");
+
+    lives = new livesCounter ("30px", "Consolas", "black", 280, 40, "text");
+
+
+    myGameArea.start();
+
+
+}
+
 var myGameArea = {
     canvas : document.getElementById("canvas"),
     start : function() {
-        this.canvas.width = 500;
+        this.canvas.width = 750;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
@@ -48,6 +75,7 @@ var myGameArea = {
         })
         window.addEventListener('keyup', function (e) {
             myGameArea.key = false;
+            myGamePiece.image.src = "Molly-Stand.png";
         })
     }, 
     stop : function() {
@@ -58,69 +86,169 @@ var myGameArea = {
     }
 }
 
-function enemy(width, height, color, x, y, type)
+
+function animateBackground(width, height, color, x, y, type)
 {
     this.type = type;
+    if (type == "image" || type == "back") {
+        this.image = new Image();
+        this.image.src = color;
+    }
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;    
     this.x = x;
-    this.y = y;  
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        if (type == "image" || type == "back") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        if (type == "back") {
+            ctx.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.type == "back") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
+    }
+
+}
+
+
+
+function end(width, height, color, x, y, type)
+{
+    this.type = type;
+    if (type == "image" || type == "back") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;
+    this.hit = false; 
 
     this.update = function(){
         ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        if (type == "image" || type == "back") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        if (type == "back") {
+            ctx.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+
+    }
+
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.type == "back") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
+        }
+
+
+
+}
+
+function final()
+{
+
+    if(endOfLevel)
+    {
+
+     endOfLevel.speedX = -1;  
+     endOfLevel.newPos(); 
+     endOfLevel.update();
+     
+
+    if(collision(myGamePiece, endOfLevel) == true)
+    {
+        console.log("end is here");
+        myGameArea.stop();
+        return;
+    }       
+    }
+
+}
+
+function enemy(width, height, color, x, y)
+{
+ 
+    this.image = new Image();
+    this.image.src = color;
+    
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.x = x;
+    this.y = y;
+    this.levelEnd = false;
+
+    this.update = function(){
+        ctx = myGameArea.context;
+     
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+  
+   
 
     }
 
         this.newPos = function() {
-        
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.type == "back") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
         }
 }
 
-/*function fly(width, height, color, x, y, type)
-{
-    this.type = type;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;  
 
-    this.update = function(){
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+function powerUp(width, height, color, x, y, powerType, type)
+{
+    this.powerType = powerType;
+    this.type = type;
+            if (type == "image" || type == "back") {
+        this.image = new Image();
+        this.image.src = color;
     }
-
-}
-
-function walk(width, height, color, x, y, type)
-{
-    this.type = type;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.x = x;
-    this.y = y;  
-
-    this.update = function(){
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }  
-}*/
-
-
-function powerUp(width, height, color, x, y, type)
-{
-    this.type = type;
     this.width = width;
     this.height = height;
     this.speedX = 0;
@@ -131,9 +259,22 @@ function powerUp(width, height, color, x, y, type)
     this.visible = true;
 
     this.update = function(){
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+          ctx = myGameArea.context;
+        if (type == "image" || type == "back") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        if (type == "background") {
+            ctx.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
 
     }
 
@@ -141,9 +282,9 @@ function powerUp(width, height, color, x, y, type)
 
         if(storedPowerUp.length == 0)
         {
-            storedPowerUp.push(type);
+            storedPowerUp.push(powerType);
             this.active = false;
-            console.log(this.type + " has been stored");
+            console.log(this.powerType + " has been stored");
             //console.log(storedPowerUp.length);
         }
 
@@ -153,6 +294,11 @@ function powerUp(width, height, color, x, y, type)
         
         this.x += this.speedX;
         this.y += this.speedY;
+        if (this.type == "back") {
+            if (this.x == -(this.width)) {
+                this.x = 0;
+            }
+        }
         }
 
 
@@ -163,6 +309,10 @@ function powerUp(width, height, color, x, y, type)
 
 function component(width, height, color, x, y, type) {
     this.type = type;
+        if (type == "image" || type == "back") {
+        this.image = new Image();
+        this.image.src = color;
+    }
     this.width = width;
     this.height = height;
     this.speedX = 0;
@@ -174,10 +324,22 @@ function component(width, height, color, x, y, type) {
     this.jumping = false;
     this.shrink = false;
     this.update = function() {
-        ctx = myGameArea.context;
-
+         ctx = myGameArea.context;
+        if (type == "image" || type == "back") {
+            ctx.drawImage(this.image, 
+                this.x, 
+                this.y,
+                this.width, this.height);
+        if (type == "background") {
+            ctx.drawImage(this.image, 
+                this.x + this.width, 
+                this.y,
+                this.width, this.height);
+        }
+        } else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
         
     }
     this.newPos = function() {
@@ -205,25 +367,20 @@ function component(width, height, color, x, y, type) {
 
 function livesCounter(width, height, color, x, y, type)
 {
-    this.type = type;
+    
     this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;    
+    this.height = height;  
     this.x = x;
     this.y = y;
 
     this.update = function()
     {
         ctx = myGameArea.context;
-        if (this.type == "text") {
+ 
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }  
+
     }
     
 
@@ -312,18 +469,19 @@ function powerUpCollision(gamePiece, powerUpType)
     }   
 }
 
-function enemyInterval()
+function walkEnemyInterval()
 {
-    flyingEnemy.push( new enemy(100, 80, "green", 300, 150));
+    walkingEnemy.push( new enemy(100, 80, "green", 300, 150));
     //walkingEnemy.push( new enemy(100, 100, "blue", 300, 450));
 }
 
 function updateGameArea() {
 
+    //animateBackground();
 
     cooldownTime();
 
-    let inter = Math.floor((Math.random() * 10000) + 9000);
+    let inter = Math.floor((Math.random() * 150) + 50);
     
 
     for(i = 0; i < flyingEnemy.length; i +=1)
@@ -366,60 +524,65 @@ function updateGameArea() {
     }
 
    
+ 
+    //setTimeout(final, 200);
+    
 
 
     myGameArea.clear();
 
     myGameArea.frameNum += 1;
 
-    if(myGameArea.frameNum == 1 || everyFlyinterval(350))
-    {
-        flyingEnemy.push( new enemy(100, 100, "green", 500, 150));
-        //walkingEnemy.push( new enemy(100, 100, "blue", 300, 450));
+    background.speedX = -1;
+    background.newPos();
+    background.update();
 
-        //setInterval(enemyInterval, inter);
-    }
-   if(myGameArea.frameNum == 1 || everyWalkinterval(350))
+    if(myGameArea.frameNum == 100 || everyFlyinterval(250))
     {
-        walkingEnemy.push( new enemy(100, 100, "blue", 700, 400));
-    }
+        flyingEnemy.push( new enemy(100, 40, "Bird.png", 2000, 360));
 
-    if(myGameArea.fameNum == 1 || everySheildInterval(350))
+    }
+   if(myGameArea.frameNum == 1 || everyWalkinterval(250))
     {
-        sheildPowerUp.push( new powerUp(50, 50, "pink", 900, 400, "sheild"));
+        walkingEnemy.push( new enemy(100, 80, "Dog.png", 850, 420));
     }
 
-    if(myGameArea.frameNum == 1 || everyJumpInterval(350))
+    if(myGameArea.frameNum == 200|| everySheildInterval(350))
     {
-        jumpPowerUp.push(new powerUp(50, 50, "purple", 200, 400, "jump"));
+        sheildPowerUp.push( new powerUp(50, 50, "Shield.png", 900, 400, "sheild", "image"));
+    }
+
+    if(myGameArea.frameNum == 300 || everyJumpInterval(350))
+    {
+        jumpPowerUp.push(new powerUp(50, 50, "Jump.png", 1000, 400, "jump", "image"));
     }
     
 
 
     for(i = 0; i < flyingEnemy.length; i += 1)
     {
-        flyingEnemy[i].speedX = -1;
+        flyingEnemy[i].speedX = -2;
         flyingEnemy[i].newPos();
         flyingEnemy[i].update();
     }
 
     for(i = 0; i < walkingEnemy.length; i += 1)
     {
-        walkingEnemy[i].speedX = -1;
+        walkingEnemy[i].speedX = -2;
         walkingEnemy[i].newPos();
         walkingEnemy[i].update();
     }
 
     for(i = 0; i < sheildPowerUp.length; i += 1)
     {
-        sheildPowerUp[i].speedX = -1;
+        sheildPowerUp[i].speedX = -1.2;
         sheildPowerUp[i].newPos();
         sheildPowerUp[i].update();
     }
 
     for(i = 0; i < jumpPowerUp.length; i += 1)
     {
-        jumpPowerUp[i].speedX = -1;
+        jumpPowerUp[i].speedX = -1.2;
         jumpPowerUp[i].newPos();
         jumpPowerUp[i].update();
     }
@@ -427,24 +590,35 @@ function updateGameArea() {
     move(myGameArea.key, "sim");
     //move(myGameArea.key, "en");
 
+
+    if(endOfLevel !== null)
+    {
+        final();
+    }
     
     //flyingEnemy.update();
     //walkingEnemy.update();
     //console.log(myGameArea.frameNum);
     //jumpPowerUp.update();
     //sheildPowerUp.update();
+    
     myGamePiece.x += myGamePiece.speedX;
     myGamePiece.y += myGamePiece.speedY;
     lives.text = "Lives: " + livesRemain;
     lives.update();
+
     myGamePiece.newPos();    
     myGamePiece.update();
+   
+
+    
 }
 
 function move(key)
 {
     if(key == 37)
     {
+        //myGamePiece.image.src = "Molly-Left.png";
         goLeft()
     }
     else if (key == 39)
@@ -473,11 +647,13 @@ function move(key)
 
 function goLeft()
 {
+    myGamePiece.image.src = "Molly-Left.png";
     myGamePiece.speedX = -2;
 }
 
 function goRight()
 {
+    myGamePiece.image.src = "Molly-Right.png";
     myGamePiece.speedX = 2;
 }
 
@@ -485,13 +661,13 @@ function jump(type)
 {
     if(type == "en")
     {
-        myGamePiece.gravitySpeed = -9.5; myGamePiece.jumping = true;
+        myGamePiece.gravitySpeed = -11.5; myGamePiece.jumping = true;
 
         
     }
     else if (type == "sim")
     {
-        myGamePiece.gravitySpeed = -6.5; myGamePiece.jumping = true;
+        myGamePiece.gravitySpeed = -8.5; myGamePiece.jumping = true;
     }
         
   
@@ -500,20 +676,12 @@ function jump(type)
 
 function superJump()
 {
-    myGamePiece.gravitySpeed = -9.5; myGamePiece.jumping = true;    
+    myGamePiece.gravitySpeed = -9; myGamePiece.jumping = true;    
 }
 
 
 
 function everyFlyinterval(n) {
-    if ((myGameArea.frameNum / n) % 1 == 0) 
-        {
-            return true;
-        }
-        return false;
-}
-
-function everyWalkinterval(n) {
     if ((myGameArea.frameNum / n) % 3 == 0) 
         {
             return true;
@@ -521,9 +689,20 @@ function everyWalkinterval(n) {
         return false;
 }
 
+function everyWalkinterval(n) {
+    
+    /*if ((myGameArea.frameNum / n) % 2 == 0) 
+        {
+            return true;
+        }
+        return false;*/
+
+        return (myGameArea.frameNum % (2 * n)) === 0;
+}
+
 function everySheildInterval(n)
 {
-    if((myGameArea.frameNum / n) % 1 == 0)
+    if((myGameArea.frameNum / n) % 5 == 0)
     {
         return true;
     }
@@ -532,13 +711,18 @@ function everySheildInterval(n)
 
 function everyJumpInterval(n)
 {
-    if((myGameArea.frameNum / n) % 2 == 0)
+    if((myGameArea.frameNum / n) % 4 == 0)
     {
         return true;
     }
     return false;
 }
 
+function clearmove() {
+    myGamePiece.image.src = "Molly-Stand.png";
+    myGamePiece.speedX = 0; 
+    myGamePiece.speedY = 0; 
+}
 var showCredits = function(){
        document.getElementById("head").style.display = "none";
        document.getElementById("credButton").style.display = "none";
